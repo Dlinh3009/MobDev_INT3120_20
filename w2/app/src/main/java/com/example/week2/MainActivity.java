d5c379b258f02169e660adb2bed170867e527165
Dlinh3009 <linhnguyenduy30092003@gmail.com>
2023-09-10 01:57:27 +0700

    slide 2 update

--- w2/app/src/main/java/com/example/week2/MainActivity.java
+++ w2/app/src/main/java/com/example/week2/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.week2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.NumberPicker;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private NumberPicker numberPicker;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        numberPicker = (NumberPicker) findViewById(R.id.numberPicker);
+        numberPicker.setMaxValue(1000);
+        numberPicker.setMinValue(900);
+        numberPicker.setValue(999);
     }
 }
